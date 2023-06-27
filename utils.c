8e5da4dc4d80a31fe5a9b63d70d9de01888e4e54
Rutendo <rutsvovera@gmail.com>
2023-06-27 12:13:31 -0700

    utils

---
+++ utils.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * is_util - defines a character.
+ * @c: character.
+ * Return: 0.
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+	return(0);
+}
+int append_hex_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEFG";
+	if (ascii_code < 0)
+		ascii_code *= -1;
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+	return (3);
+}
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+long in convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+	return ((int)num);
+}
