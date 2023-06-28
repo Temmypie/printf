47fbba1c5133138cab5a09bb58ca2c99686d846c
Tendo <122873127+TendoVera@users.noreply.github.com>
2023-06-28 21:16:04 +0200

    Update main.h

--- main.h
+++ main.h
@@ -40,8 +40,7 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i, char buffer[], int flags, int width, int precision, int size);
 
 /* Funtions for printing chars and strings */
 int print_char(va_list types, char buffer[],
@@ -62,7 +61,7 @@ int print_oct(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hex(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int print_hexa_upper(va_list types, char buffer[],
+int print_hex_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 int print_hexa(va_list types, char map_to[],
@@ -96,9 +95,9 @@ int handle_write_char(char c, char buffer[],
 int write_number(int is_positive, int ind, char buffer[],
 	int flags, int width, int precision, int size);
 int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
+	int length, char pad, char ext_ch);
 int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
+	int width, int flags, char pad, char ext_ch, int padd_start);
 
 int write_unsgnd(int is_negative, int ind,
 char buffer[],
